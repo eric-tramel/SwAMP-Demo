f89e1157faa14091c6865e17a12bdf2b4de8a401
Florent Krzakala <florent.krzakala@gmail.com>
2014-10-22 12:02:00 +0200

    Some missing build paramters were causing setup.m to not run properly.

--- matlab/setup.m
+++ matlab/setup.m
@@ -2,10 +2,11 @@ function setup
     newline = sprintf('\n');
     src = regexprep(ls('src/*/*.c'), '[ \t\n]+', ' ');
     cxx = 'mex';
-    cflags = '-largeArrayDims';
+    cflags = '-g -largeArrayDims -ldl CFLAGS="\$CFLAGS -std=c99"';
 
     fprintf('Compiling SwAMP... ')
-    eval([cxx ' ' cflags ' -output bin/swamp src/swamp.c ' src])
-    eval([cxx ' ' cflags ' -output bin/swgamp src/swgamp.c ' src]);
-    fprintf('Done!\n')
+    
+    eval([cxx ' ' cflags ' -output bin/swamp src/swamp.c ' src]);
+    eval([cxx ' ' cflags ' -output bin/swgamp src/swgamp.c ' src]);   
+fprintf('Done!\n')
 end
